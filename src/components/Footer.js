f7ac346f9440d9de9cfa949ada14cfd04fe62dd9
pritika mishra <pritikamishra2017@gmail.com>
2023-02-12 15:32:35 +0530

    nav and footer styling done

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,14 @@
 import React from "react";
+import "../styles/footer.css"
 
 const Footer = () => {
     return (
         <footer>
-            <img src={require("../assets/footer_logo.png")} alt="" />
-            <menu>
-                <div>
+            <div className="footer-logo">
+                <img src={require("../assets/footer-logo.png")} alt="" />
+            </div>
+            <div className="footer-links" id="nav">
+                <div className="navigation">
                     <h2>Navigation</h2>
                     <ul>
                         <li>
@@ -15,45 +18,42 @@ const Footer = () => {
                             <a href="#">About</a>
                         </li>
                         <li>
-                            <a href="#">Menu</a>
-                        </li>
-                        <li>
                             <a href="#">Reservations</a>
                         </li>
                         <li>
-                            <a href="#">Order Online</a>
-                        </li>
-                        <li>
-                            <a href="#">Login</a>
+                            <a href="#">Order</a>
                         </li>
                     </ul>
                 </div>
-                <div>
+            </div>
+            <div className="footer-links" id="contact">
+                <div className="contact">
                     <h2>Contact</h2>
-                    <ul>
-                        <li>2395 Maldove Way,</li>
-                        <li>Chicago Illinois</li>
-                        <br />
-                        <li>(629)-243-6827</li>
-                        <br />
-                        <a href="#">info@littlelemon.com</a>
-                    </ul>
+                    <p>2395 Maldove Way</p>
+                    <p>Chicago Illinois</p>
+                    <p>(629)-243-6827</p>
+                    <a href="#">info@littlelemon.com</a>
                 </div>
-                <div>
+            </div>
+            <div className="footer-links" id="connect">
+                <div className="connect">
                     <h2>Connect</h2>
                     <ul>
-                        <li>
-                            <a href="#">Instagram</a>
-                        </li>
                         <li>
                             <a href="#">Facebook</a>
                         </li>
                         <li>
-                            <a href="#">Linkedin</a>
+                            <a href="#">Instagram</a>
+                        </li>
+                        <li>
+                            <a href="#">Likedin</a>
+                        </li>
+                        <li>
+                            <a href="#">Join Us!</a>
                         </li>
                     </ul>
                 </div>
-            </menu>
+            </div>
         </footer>
     )
 }
