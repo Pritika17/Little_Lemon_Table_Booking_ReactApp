53e91f0b940fa3c91d63e3d1e0b5b0d465bab01f
pritika mishra <pritikamishra2017@gmail.com>
2023-02-17 16:52:05 +0530

    homepage ready

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,16 +1,16 @@
 import React from "react";
 import About from "./Homepage/About";
-// import Restaurant from "./Homepage/about-restaurant";
+import Restaurant from "./Homepage/about-restaurant";
 import Specials from "./Homepage/Specials/specials";
-// import Testimonial from "./Homepage/Testimonials/testimonials";
+import Testimonial from "./Homepage/Testimonials/testimonials";
 
 const Main = () => {
     return (
         <main>
             <About />
             <Specials/>
-            {/* <Testimonial/>
-            <Restaurant/> */}
+            <Testimonial/>
+            <Restaurant/>
         </main>
     )
 }
